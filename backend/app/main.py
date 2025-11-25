919213db39ca2e8e5390815fb8b32e3e0214f52c
ryywh45 <ryywh45@gmail.com>
2025-11-25 11:21:16 +0000

    chore: CORS setup for development

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import SQLiteDB
 from app.api.v1.api import api_router
 
 
 app = FastAPI()
 
+# 開發環境用
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 設為 ["*"] 則允許所有來源
+    allow_credentials=True, # 允許使用憑證（如 Cookies、HTTP 身份驗證）。如果為 True，則 allow_origins 不能設定為 ["*"]
+    allow_methods=["*"],    # 允許所有方法：GET, POST, OPTIONS, etc.
+    allow_headers=["*"],    # 允許所有標頭，包括 'Content-Type'
+)
 
 @app.on_event("startup")
 def on_startup():
